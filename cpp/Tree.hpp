3671a70d21ffbbbdc700b7646ec247e973a8bee0
Ubuntu <cs561user@Project1-Machine.zk1eckxwzyeu1fmoez5vkeqyka.bx.internal.cloudapp.net>
2022-04-14 01:44:16 +0000

    basic serialization format done

--- cpp/Tree.hpp
+++ cpp/Tree.hpp
@@ -3,33 +3,46 @@
 #include <random>
 #include <cassert>
 #include <iostream>
+#include <cstring>
+#include "TreeSerializer.hpp"
 
 #pragma once
 
 struct Node
 {
-    Node(size_t branchingFactor){
-        maxChildren = branchingFactor;
-        key = -1;
-	children.reserve(branchingFactor);
-    }
+    Node(size_t branchingFactor) : maxChildren(branchingFactor), numChildren(0), maxValues(branchingFactor), numValues(0), key(-1) {}
     size_t maxChildren;
+    size_t numChildren;
+    size_t maxValues;
+    size_t numValues;
     int key;
-    std::vector<int> values;
-    std::vector<Node*> children;
+    int values[8];
+    Node* children[8];
 };
-
-struct dummy_node { int x, int y, int z };
 
 class Tree{
     public:
-        Tree(size_t branchingFactor){
-            branch = branchingFactor;
-        }
+        Tree(size_t branchingFactor) : branch(branchingFactor), head(nullptr), ts_init(false) {}
 
         ~Tree(){
 
         }
+
+	void init_serializer(std::string filename){
+		TS.openFile(filename, MODE::WRITE);
+		ts_init = true;
+	}
+	S_Node* node_to_snode(Node* node){ //returns a pointer declared by new, user should delete
+		S_Node* s_node = new S_Node();
+		s_node->id = node->key;
+			
+		std::memcpy(s_node->payloads, node->values, node->numValues);	
+		
+		for (size_t i = 0; i < node->numChildren; ++i){	
+			s_node->children[i] = node->children[i]->key;
+		}
+		return s_node;	
+	}
         void fill(long numInserts, long upperBound, std::vector<int>& keys, std::vector<int>& values){
             std::random_device rd;
             std::mt19937 gen(rd());
@@ -55,10 +68,10 @@ class Tree{
                 if (currentNode == nullptr) return std::vector<int>();
                 if (currentNode->key == key){
                     std::vector<int> ret;
-                    ret.insert(ret.begin(), (*currentNode).values.begin(), (*currentNode).values.end());
+                    ret.insert(ret.begin(), currentNode->values, currentNode->values + currentNode->numChildren);
                     return ret;
                 } else{
-                    queue.insert(queue.end(), currentNode->children.begin(), currentNode->children.end());
+                    queue.insert(queue.end(), currentNode->children, currentNode->children + currentNode->numChildren);
                 }
             }
         }
@@ -74,15 +87,27 @@ class Tree{
                     Node* currentNode = queue.front(); queue.pop_front();
                     if (currentNode == nullptr) return std::vector<int>();
                     if (currentNode->key == key){
-                        // std::vector<int> ret;
-                        // ret.insert(ret.begin(), (*currentNode).values.begin(), (*currentNode).values.end());
-                        return currentNode->values;
+                        std::vector<int> ret;
+                        ret.insert(ret.begin(), currentNode->values, currentNode->values + currentNode->numValues);
+                        return ret;
                     } else{
-                        queue.insert(queue.end(), currentNode->children.begin(), currentNode->children.end());
+                        queue.insert(queue.end(), currentNode->children, currentNode->children + currentNode->numChildren);
                     }
                 }
             }
         }
+
+	void dump_tree(){
+		dump_tree_impl(this->head);
+	}
+
+	const Node* get_head_ref(){
+		return head;
+	}
+
+	S_Node* digestNode(){
+		return TS.readNode();
+	}
 
     private:
         void add_impl(int key, int value){
@@ -90,8 +115,7 @@ class Tree{
             if (head == nullptr){
                 head = new Node(branch);
                 head->key = key;
-                head->values.push_back(value);
-                head->children = std::vector<Node*>();
+                head->values[head->numValues++] = value;
                 return;
             }
             queue.push_back(head);
@@ -101,21 +125,38 @@ class Tree{
                 if (currentNode == nullptr){
                     assert(false && "unreachable");
                 } else if (currentNode->key == key){
-                    currentNode->values.push_back(value);
+		    if (currentNode->numValues < currentNode->maxValues) currentNode->values[currentNode->numValues++] = value;
                     return;
-                } else if (currentNode->children.size() < branch){
+                } else if (currentNode->numChildren < branch){
                     Node* node = new Node(branch);
                     node->key = key;
-                    node->values.push_back(value);
-                    currentNode->children.push_back(node);
+		    node->values[node->numValues++] = value;
+                    currentNode->children[currentNode->numChildren++] = node;
                     return;
                 } else {
-                    queue.insert(queue.end(), currentNode->children.begin(), currentNode->children.end());
+                    queue.insert(queue.end(), currentNode->children, currentNode->children + currentNode->numChildren);
                 }
             }
         }
+
+	void dump_tree_impl(Node* head){
+		if (!ts_init){ 
+			std::cerr << "[ERROR] Serializer was not initialized" << std::endl;
+			exit(1);
+		}
+		if (!head) return;
+		auto s_node_ptr = node_to_snode(head);
+		TS.writeNode(s_node_ptr);
+		delete s_node_ptr;
+		for (size_t i=0; i < head->numChildren; ++i){	
+			dump_tree_impl(head->children[i]);
+		}	
+
+	}
         Node* head;
         size_t numElements;
         size_t branch;
+	TreeSerializer TS;
+	bool ts_init;
 };
 
--- cpp/TreeSerializer.cpp
+++ cpp/TreeSerializer.cpp
@@ -1,55 +1,60 @@
-#define _GNU_SOURCE
-
 #include "Tree.hpp"
 #include "TreeSerializer.hpp"
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <iostream>
 #include <vector>
+
+#define align(p, a) (((long)(p) + (a - 1)) & ~(a - 1))
 
 	/*
 	 * NOTE: The size of a page on this device is 4096 bytes
 	 */	
-	TreeSerializer::TreeSerializer(std::string filename, MODE mode){
-			int flags = O_DIRECT | O_SYNC;
-			switch(mode){
-				case MODE::READ: //READ
-					flags |= O_RDONLY;
-					mode_internal = mode;
-					break;
-				case MODE::WRITE: //WRITE
-					flags |= O_RDWR | O_CREAT;
-					mode_internal = mode;
-					break;
-				default: break;
-			fd = open((filename + ".treefile").c_str(), flags);
-			if (fd < 0) {
-				std::cerr << "[ERROR]: Serializer failed\n[MESSAGE]: file unable to be opened or created";
-				exit(1);
-			}
-		}
-	}
+	TreeSerializer::TreeSerializer() : at_eof(false), fd(-1), mode_internal(MODE::INVALID), read_offset(0), offset(0) {}
 
 	TreeSerializer::~TreeSerializer(){
 		close(fd);
 	}
 
-	void TreeSerializer::writeNode(Node* node){
-		write(fd, (void*)node, sizeof(*node));
+	void TreeSerializer::openFile(std::string filepath, MODE mode){
+	int flags = O_DIRECT | O_SYNC;
+			switch(mode){
+				case MODE::READ: //READ
+					flags |= O_RDONLY;
+					this->mode_internal = mode;
+					break;
+				case MODE::WRITE: //WRITE
+					flags |= O_RDWR | O_CREAT;
+					this->mode_internal = mode;
+					break;
+				default: break;
+			}
+			this->fd = open(filepath.c_str(), O_RDWR | O_CREAT);
+			if (this->fd < 0) {
+				std::cerr << "[ERROR]: Serializer failed\n[MESSAGE]: file unable to be opened or created";
+				exit(1);
+			}
 	}
 
-	Node* TreeSerializer::readNode(){
-		Node* node = (Node*)malloc(sizeof(Node));
-		read(fd, (void*)node, sizeof(Node));
+	void TreeSerializer::writeNode(S_Node* node){
+		ssize_t bytes = pwrite(this->fd, (char*)node, sizeof(S_Node), this->offset);
+		std::cout << "Bytes written: " << bytes << std::endl;
+		if (bytes <= 0) { 
+			std::cout << "Could not write\nReason: " + std::string(strerror(errno))<<std::endl;
+			return;
+		}
+		this->offset += bytes;
+	}
+
+	S_Node* TreeSerializer::readNode(){
+		S_Node* node = new S_Node();
+		ssize_t bytes = pread(this->fd, (char*)node, sizeof(S_Node), read_offset);
+		std::cout << "read node " << node->id << std::endl;
+		std::cout << "Bytes read: " << bytes << std::endl;
+		if(bytes<=0) std::cout << "Could not Read\nReason: " + std::string(strerror(errno))<<std::endl;
 		return node;
 	}
-int main(int argc, char** argv){
-	TreeSerializer TS("test", MODE::WRITE);
-	Node* node = new Node(8);
-    dummy_node d_node = dummy_node{.x = 1, .y = 2, .z = 3};
-	node->key = 12;
-	TS.writeNode(&d_node);
-	dummy_node* second_node = TS.readNode();
-	std::cout << second_node->x;
-	return 0;
-}
+
