c1b52b3bfc0edfa6abaa7243bcade86343121ec4
Spin Eve <20128868+SpinEve@users.noreply.github.com>
2022-07-18 21:19:37 -0400

    Init reader function.

---
+++ cpp/Reader.hpp
@@ -0,0 +1,14 @@
+#include "Serializer.hpp"
+#include "Tree.hpp"
+#include "Graph.hpp"
+#include <cstring>
+
+#pragma once
+
+void readData(std::string path){
+  Serializer sz;
+  sz.openFile(path, MODE::READ);
+  // Determine which data structure it is
+  // Initialize correspond data structure
+  // and return pointer (or object?)
+}
--- cpp/Utils.hpp
+++
@@ -1,6 +0,0 @@
-#include <vector>
-#include <random>
-
-void random_fill(std::vector<int>& vec){
-
-}
